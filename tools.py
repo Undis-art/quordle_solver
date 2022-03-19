2870750203b36ceda4e5939980db7af832eaf8cf
Miia Lillstrang <miia.lillstrang@gmail.com>
2022-03-20 00:01:08 +0200

    Tools to mark matching letters and update wordle state.

--- tools.py
+++ tools.py
@@ -1,7 +1,10 @@
 import json
 from string import ascii_lowercase as letters
 
+import numpy as np
+
 def all_words(n=5):
+    """ List all n letters long words in dictionary."""
 
     # Load dictionary
     with open("dictionary.json", "r") as f:
@@ -17,3 +20,65 @@ def all_words(n=5):
     nrdle_words = [w for w in words_len5 if all([c in letters for c in w])]
 
     return nrdle_words
+
+def new_state(state, word, guess):
+    """
+    Generate updated state for wordle guesses according to previous state,
+    the word (to be guessed) and given guess.
+    """
+
+    # Make the new row of info
+    new_row = np.array([match(guess, word)])
+
+    return np.concatenate((state, new_row), axis=0)
+    
+def match(guess, word):
+    """
+    Describe correctness of word in argument "guess" compared to word in
+    argument "word".
+    0: Letter doesn't appear in word.
+    1: Letter appears in the word, but in different location.
+    2: Letter appears in the word, and in the same location.
+    """
+
+    n = len(word)
+
+    # Use numpy arrays for convenience
+    word_arr = np.array(list(word))
+    guess_arr = np.array(list(guess))
+    
+    # Initialize with all zeros
+    match = np.array([0] * n)
+
+    # Mark right letters in right places
+    right_place = word_arr == guess_arr
+    match[right_place] = 2
+
+    def _all_marked(l):
+        """
+        Tell whether a letter has already been marked in match-array as many
+        times as it occurs in word.
+        """
+        # List letters that have been marked with 2 or 1 so far
+        all_marked_letters = guess_arr[match != 0]
+
+        # Count previously marked occurrences of l
+        n_marked = np.array(all_marked_letters == l).sum()
+        
+        # Count occurrences of l in word
+        n_in_word = (word_arr == l).sum()
+
+        if n_marked == n_in_word:
+            return True
+        else:
+            return False
+
+    not_exact_matches = np.where(match != 2)[0]
+
+    # Mark right letters in wrong places
+    for i in not_exact_matches:
+        if guess_arr[i] in word_arr and not _all_marked(guess[i]):
+            match[i] = 1
+
+    return match
+
--- wordle_class.py
+++ wordle_class.py
@@ -5,12 +5,14 @@ Wordle object class
 from random import choice
 import tools
 
+import numpy as np
+
 
 class Wordle:
 
     def __init__(self, letters=5):
         self.letters = letters
-        self.state =  [0] * letters
+        self.state =  np.array([[0] * letters])
         self.all_words = tools.all_words(letters)
         self.word = choice(self.all_words) 
         self.possible_words = self.all_words
@@ -19,6 +21,7 @@ class Wordle:
         """
         update self.state according to guess.
         """
+        self.state = tools.new_state(self.state, self.word, guess)
      
     def update_possible_words(self, guess):
         """
